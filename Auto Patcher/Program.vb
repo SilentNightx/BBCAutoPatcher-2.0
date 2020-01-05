2e49dd6f9352e6e5eb027321856f93a9fa862298
SilentNightx <39037428+SilentNightx@users.noreply.github.com>
2020-01-04 21:40:41 -0500

    Added load order conversion and error checking

--- Auto Patcher/Program.vb
+++ Auto Patcher/Program.vb
@@ -9,10 +9,10 @@ Module Program
 
     'Path to openmw.cfg
     Public cfgPath As String
+    'Path to Morrowind.ini
+    Public iniPath As String
     'Path to config for this program (BBCAutoPatcher.cfg) which stores the cfgPath.
     Public localCfgPath As String
-    'Answer to Yes/No prompt
-    Public answer As String
 
     Sub Main(args As String())
         Console.WriteLine("-------------------------------------------------------------------------------------------------------------------")
@@ -42,9 +42,8 @@ Module Program
             Console.WriteLine("   can't read OpenMW load orders.")
         End If
         Console.WriteLine("-------------------------------------------------------------------------------------------------------------------")
-        Console.Write("Press any key to continue...")
+        Console.WriteLine("Press any key to continue...")
         Console.ReadKey(True)
-        Console.WriteLine("")
 
         If cfgPath <> "No" Then
             ConvertLoadOrder()
@@ -58,14 +57,82 @@ Module Program
 
         Console.WriteLine("Converting load order...")
 
-        Dim lines = File.ReadAllLines(cfgPath)
-        For Each line In lines
-            If line.StartsWith("content=") Then
+        iniPath = FileSystem.CurrentDirectory
+        iniPath = iniPath.Remove(iniPath.Length - 10)
+        iniPath += "Morrowind.ini"
 
-                'Add load order conversion code here. The program first warns that it will copy OpenMW load order to vanilla if running OpenMW. It's safe to edit and overwrite the file here.
+        If System.IO.File.Exists(iniPath) And System.IO.File.Exists(cfgPath) Then
+
+            If File.ReadAllText(iniPath).Length = 0 Then
+
+                Console.WriteLine("Error, Morrowind.ini is empty. Please run the game once.")
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey(True)
+                Environment.Exit(-1)
+
+            ElseIf File.ReadAllText(cfgPath).Length = 0 Then
+
+                Console.WriteLine("Error, openmw.cfg is empty. Please run OpenMW once.")
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey(True)
+                Environment.Exit(-1)
+
+
+            Else
+
+                Dim newLines = From line In File.ReadAllLines(iniPath) Where Not line.StartsWith("GameFile")
+                File.WriteAllLines(iniPath, newLines)
+
+                Dim newLines2 = From line In File.ReadAllLines(iniPath) Where Not line.StartsWith("[Game Files]")
+                File.WriteAllLines(iniPath, newLines2)
+
+                Using sw As New StreamWriter(File.Open(iniPath, FileMode.Append))
+                    sw.WriteLine("[Game Files]")
+                End Using
+
+                Dim lines = File.ReadAllLines(cfgPath)
+                Dim fileNo As Integer = -1
+                For Each line In lines
+                    If line.StartsWith("content=") Then
+                        fileNo += 1
+                        Dim splitLine As String() = line.Split("=")
+                        Dim formattedLine = "GameFile" + fileNo.ToString + "=" + splitLine(1)
+
+                        Using sw As New StreamWriter(File.Open(iniPath, FileMode.Append))
+                            sw.WriteLine(formattedLine)
+                        End Using
+
+                    End If
+                Next
+
+                Console.WriteLine("Load order converted.")
 
             End If
-        Next
+
+        ElseIf System.IO.File.Exists(cfgPath) Then
+
+            Console.WriteLine("Error, Morrowind.ini doesn't exist. Please run the game once and make sure you placed this program in your")
+            Console.WriteLine("Data Files directory.")
+            Console.WriteLine("Press any key to continue...")
+            Console.ReadKey(True)
+            Environment.Exit(-1)
+
+        ElseIf System.IO.File.Exists(iniPath) Then
+
+            Console.WriteLine("Error, saved openmw.cfg doesn't exist. Please delete BBCAutoPatcher.cfg and rerun to reconfigure the program.")
+            Console.WriteLine("Press any key to continue...")
+            Console.ReadKey(True)
+            Environment.Exit(-1)
+
+        Else
+
+            Console.WriteLine("Error, Morrowind.ini and openmw.cfg don't exist. Please run the game once and make sure you placed this program")
+            Console.WriteLine("in your Data Files directory. After that, delete BBCAutoPatcher.cfg and rerun to reconfigure the program.")
+            Console.WriteLine("Press any key to continue...")
+            Console.ReadKey(True)
+            Environment.Exit(-1)
+
+        End If
 
     End Sub
 
@@ -74,18 +141,53 @@ Module Program
         Console.WriteLine("Generating patch...")
 
         If RuntimeInformation.IsOSPlatform(OSPlatform.Windows) Then
-            Process.Start("CMD", "/K MKDIR bbcbackups & tes3cmd modify -backup-dir bbcbackups -hide-backups -program BBCAutoPatcher.pl && tes3cmd header -backup-dir bbcbackups -hide-backups -synchronize BBC_Auto_Patch.esp && RMDIR bbcbackups /s /q & ECHO ------------------------------------------------------------------------------------------------------------------- && ECHO Done! && ECHO Your load order patch was saved as BBC_Auto_Patch.esp. && ECHO Activate and load it after everything else. && ECHO ------------------------------------------------------------------------------------------------------------------- && PAUSE && EXIT")
+            If System.IO.File.Exists(FileSystem.CurrentDirectory + "\BBCAutoPatcher.pl") And System.IO.File.Exists(FileSystem.CurrentDirectory + "\tes3cmd.exe") Then
+                Process.Start("CMD", "/K MKDIR bbcbackups & tes3cmd modify -backup-dir bbcbackups -hide-backups -program BBCAutoPatcher.pl && tes3cmd header -backup-dir bbcbackups -hide-backups -synchronize BBC_Auto_Patch.esp && RMDIR bbcbackups /s /q & ECHO ------------------------------------------------------------------------------------------------------------------- && ECHO Done! && ECHO Your load order patch was saved as BBC_Auto_Patch.esp. && ECHO Activate and load it after everything else. && ECHO ------------------------------------------------------------------------------------------------------------------- && PAUSE && EXIT")
+            ElseIf System.IO.File.Exists(FileSystem.CurrentDirectory + "\BBCAutoPatcher.pl") Then
+                Console.WriteLine("Error, missing tes3cmd.exe in your Data Files directory.")
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey(True)
+                Environment.Exit(-1)
+            ElseIf System.IO.File.Exists(FileSystem.CurrentDirectory + "\tes3cmd.exe") Then
+                Console.WriteLine("Error, missing BBCAutoPatcher.pl in your Data Files directory.")
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey(True)
+                Environment.Exit(-1)
+            Else
+                Console.WriteLine("Error, missing tes3cmd.exe and BBCAutoPatcher.pl in your Data Files directory.")
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey(True)
+                Environment.Exit(-1)
+            End If
         End If
 
         If RuntimeInformation.IsOSPlatform(OSPlatform.Linux) Then
-            Process.Start("/bin/bash", "-c ""mkdir bbcbackups || true && ./tes3cmd modify -backup-dir bbcbackups -hide-backups -program BBCAutoPatcher.pl && ./tes3cmd header -backup-dir bbcbackups -hide-backups -synchronize BBC_Auto_Patch.esp && rm -rf bbcbackups  || true && echo ------------------------------------------------------------------------------------------------------------------- && echo Done! && echo Your load order patch was saved as BBC_Auto_Patch.esp. && echo Activate and load it after everything else. && echo -------------------------------------------------------------------------------------------------------------------""")
+            If System.IO.File.Exists(FileSystem.CurrentDirectory + "/BBCAutoPatcher.pl") And System.IO.File.Exists(FileSystem.CurrentDirectory + "/tes3cmd.exe") Then
+                Process.Start("/bin/bash", "-c ""mkdir bbcbackups || true && ./tes3cmd modify -backup-dir bbcbackups -hide-backups -program BBCAutoPatcher.pl && ./tes3cmd header -backup-dir bbcbackups -hide-backups -synchronize BBC_Auto_Patch.esp && rm -rf bbcbackups  || true && echo ------------------------------------------------------------------------------------------------------------------- && echo Done! && echo Your load order patch was saved as BBC_Auto_Patch.esp. && echo Activate and load it after everything else. && echo -------------------------------------------------------------------------------------------------------------------""")
+            ElseIf System.IO.File.Exists(FileSystem.CurrentDirectory + "/BBCAutoPatcher.pl") Then
+                Console.WriteLine("Error, missing tes3cmd.exe in your Data Files directory.")
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey(True)
+                Environment.Exit(-1)
+            ElseIf System.IO.File.Exists(FileSystem.CurrentDirectory + "/tes3cmd.exe") Then
+                Console.WriteLine("Error, missing BBCAutoPatcher.pl in your Data Files directory.")
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey(True)
+                Environment.Exit(-1)
+            Else
+                Console.WriteLine("Error, missing tes3cmd.exe and BBCAutoPatcher.pl in your Data Files directory.")
+                Console.WriteLine("Press any key to continue...")
+                Console.ReadKey(True)
+                Environment.Exit(-1)
+            End If
         End If
-
     End Sub
 
     Sub FirstRun()
 
         Console.WriteLine("Preforming initial setup...")
+
+        Dim answer As String
 
         If RuntimeInformation.IsOSPlatform(OSPlatform.Windows) Then
             cfgPath = SpecialDirectories.MyDocuments + "\My Games\OpenMW\openmw.cfg"
@@ -97,7 +199,7 @@ Module Program
                 Console.WriteLine("OpenMW configuration file not detected. Do you use OpenMW? (Y/N)")
                 answer = Console.ReadLine()
                 If LCase(answer) = "y" Or LCase(answer) = "yes" Then
-                    Console.WriteLine("Enter the path to your openmw.cfg file:")
+                    Console.WriteLine("Enter the full path to your openmw.cfg file:")
                     cfgPath = Console.ReadLine()
                     If System.IO.File.Exists(cfgPath) Then
                         Console.WriteLine("OpenMW configuration file detected.")
@@ -105,7 +207,7 @@ Module Program
                         FileSystem.WriteAllText(localCfgPath, cfgPath, True)
                     Else
                         Console.WriteLine("Error, no openmw.cfg detected.")
-                        Console.Write("Press any key to close...")
+                        Console.WriteLine("Press any key to close...")
                         Console.ReadKey(True)
                         Environment.Exit(-1)
                     End If
@@ -115,7 +217,7 @@ Module Program
                     FileSystem.WriteAllText(localCfgPath, cfgPath, True)
                 Else
                     Console.WriteLine("Error, invalid input.")
-                    Console.Write("Press any key to close...")
+                    Console.WriteLine("Press any key to close...")
                     Console.ReadKey(True)
                     Environment.Exit(-1)
                 End If
@@ -148,7 +250,7 @@ Module Program
                         FileSystem.WriteAllText(localCfgPath, cfgPath, True)
                     Else
                         Console.WriteLine("Error, no openmw.cfg detected.")
-                        Console.Write("Press any key to continue...")
+                        Console.WriteLine("Press any key to continue...")
                         Console.ReadKey(True)
                         Environment.Exit(-1)
                     End If
@@ -158,7 +260,7 @@ Module Program
                     FileSystem.WriteAllText(localCfgPath, cfgPath, True)
                 Else
                     Console.WriteLine("Error, invalid input.")
-                    Console.Write("Press any key to continue...")
+                    Console.WriteLine("Press any key to continue...")
                     Console.ReadKey(True)
                     Environment.Exit(-1)
                 End If
